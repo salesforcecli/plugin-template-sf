74f559e0d75c032a3bb876f2e1af218cfed7978e
Mike Donnalley <mdonnalley@salesforce.com>
2023-11-17 08:30:42 -0700

    feat!: migrate to ESM
    BREAKING CHANGES: ESM and node 18 minimum

--- test/commands/hello/world.test.ts
+++ test/commands/hello/world.test.ts
@@ -4,10 +4,10 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { TestContext } from '@salesforce/core/lib/testSetup';
+import { TestContext } from '@salesforce/core/lib/testSetup.js';
 import { expect } from 'chai';
 import { stubSfCommandUx } from '@salesforce/sf-plugins-core';
-import World from '../../../src/commands/hello/world';
+import World from '../../../src/commands/hello/world.js';
 
 describe('hello world', () => {
   const $$ = new TestContext();
